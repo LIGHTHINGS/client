10e5fe79194da908068c420699f904a0e1a9c93a
Dare Omotayo <dareomotayo32@gmail.com>
2022-08-18 21:13:40 +0100

    initial commit

---
+++ src/style.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles(()=>({
+    appBar: {
+    borderRadius: 15,
+    display: 'flex',
+    margin: '30px 0',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    aligItems: 'center'
+    }
+}));
+
+// makeStyles();
